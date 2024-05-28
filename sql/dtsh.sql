1d9b7a1a308099db2913cf4985fbb5c6519965e7
CitrusTrail <287237317@qq.com>
2024-05-28 20:03:14 +0800

    版本2

---
+++ sql/dtsh.sql
@@ -0,0 +1,240 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50737
+ Source Host           : localhost:3306
+ Source Schema         : dtsh
+
+ Target Server Type    : MySQL
+ Target Server Version : 50737
+ File Encoding         : 65001
+
+ Date: 23/05/2024 18:00:32
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50),
+  `password` varchar(50),
+  `point` int(11), -- 积分
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'chen001', '123456', 233);
+INSERT INTO `user` VALUES (2, 'chen002', '123456', 999);
+
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50),
+  `password` varchar(50),
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+INSERT INTO `admin` VALUES (1, 'chen001', '123456');
+INSERT INTO `admin` VALUES (2, 'chen002', '123456');
+
+-- ----------------------------
+-- Table structure for task
+-- ----------------------------
+DROP TABLE IF EXISTS `task`;
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255),
+  `description` text,
+  `carbon` decimal(10, 2),  -- 减少的碳排放量
+  `point` int(11), -- 积分
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of task
+-- ----------------------------
+INSERT INTO `task` VALUES (1, 'task001', '123456', 999.99, 999);
+INSERT INTO `task` VALUES (2, 'task002', '123456', 999.99, 999);
+
+-- ----------------------------
+-- Table structure for user_task
+-- ----------------------------
+DROP TABLE IF EXISTS `user_task`;
+CREATE TABLE `user_task` (
+  `user_id` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `completed_time` datetime,
+  PRIMARY KEY (`user_id`, `task_id`) USING BTREE
+);
+
+-- ----------------------------
+-- Records of user_task
+-- ----------------------------
+INSERT INTO `user_task` VALUES (1, 1, NULL);
+INSERT INTO `user_task` VALUES (2, 1, NULL);
+
+-- ----------------------------
+-- Table structure for travel
+-- ----------------------------
+DROP TABLE IF EXISTS `travel`;
+CREATE TABLE `travel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `travel_start-time` datetime,
+  `travel_end-time` datetime,
+  `travel_total-time` datetime,
+  `travel_mode` varchar(50),  -- 出行方式，如：步行、骑行、公交等
+  `carbon` decimal(10, 2),  -- 此次出行的减少的碳排放量
+  `description` text,  -- 路线详情或其他备注
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of travel
+-- ----------------------------
+INSERT INTO `travel` VALUES (1, 1, NULL, NULL, NULL, '骑行', 999.99, NULL);
+INSERT INTO `travel` VALUES (2, 2, NULL, NULL, NULL, '骑行', 999.99, NULL);
+
+-- ----------------------------
+-- Table structure for group
+-- ----------------------------
+DROP TABLE IF EXISTS `group`;
+CREATE TABLE `group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(255),
+  `group_description` text,
+  `created_by` int(11),
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of group
+-- ----------------------------
+INSERT INTO `group` VALUES (1, 'group001', NULL, 1);
+INSERT INTO `group` VALUES (2, 'group002', NULL, 1);
+
+-- ----------------------------
+-- Table structure for user_group
+-- ----------------------------
+DROP TABLE IF EXISTS `user_group`;
+CREATE TABLE `user_group` (
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`, `group_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_group
+-- ----------------------------
+INSERT INTO `user_group` VALUES (1, 1);
+INSERT INTO `user_group` VALUES (2, 1);
+
+-- ----------------------------
+-- Table structure for chat
+-- ----------------------------
+DROP TABLE IF EXISTS `chat`;
+CREATE TABLE `chat` (
+  `group_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_content` text,
+  `posted_at` datetime NOT NULL,
+  PRIMARY KEY (`group_id`, `user_id`, `posted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of chat
+-- ----------------------------
+INSERT INTO `chat` VALUES (1, 1, '666', '2024-05-01 12:00:00');
+INSERT INTO `chat` VALUES (2, 2, '123', '2024-05-01 12:00:00');
+
+-- ----------------------------
+-- Table structure for share
+-- ----------------------------
+DROP TABLE IF EXISTS `share`;
+CREATE TABLE `share` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `post_content` text,
+  `posted_at` datetime NOT NULL,
+  `likes` INT DEFAULT 0,  -- 点赞数
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of share
+-- ----------------------------
+INSERT INTO `share` VALUES (1, 1, '666', '2024-05-01 12:00:00', 2);
+INSERT INTO `share` VALUES (2, 2, '123', '2024-05-01 12:00:00', 4);
+
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50),
+  `point` int(11),
+  `stock` int(11), -- 库存
+  `description` text,  -- 备注
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+INSERT INTO `goods` VALUES (1, 'good001', 999, 999, NULL);
+INSERT INTO `goods` VALUES (2, 'good002', 999, 999, NULL);
+
+-- ----------------------------
+-- Table structure for buy
+-- ----------------------------
+DROP TABLE IF EXISTS `buy`;
+CREATE TABLE `buy` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11),
+  `goods_id` int(11),
+  `time` datetime,
+  `point` int(11),
+  `num` int(11),
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of buy
+-- ----------------------------
+INSERT INTO `buy` VALUES (1, 1, 1, '2024-05-01 12:00:00', 999, 999);
+INSERT INTO `buy` VALUES (2, 1, 1, '2024-05-01 12:00:00', 999, 999);
+
+-- ----------------------------
+-- Table structure for user_carbon
+-- ----------------------------
+DROP TABLE IF EXISTS `user_carbon`;
+CREATE TABLE `user_carbon` (
+  `user_id` int(11) NOT NULL,
+  `date` date,
+  `carbon` int(11),
+  PRIMARY KEY (`user_id`, `date`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_carbon
+-- ----------------------------
+INSERT INTO `user_carbon` VALUES (1, '2024-05-01', 999);
+INSERT INTO `user_carbon` VALUES (1, '2024-05-02', 999);
+
+SET FOREIGN_KEY_CHECKS = 1;
