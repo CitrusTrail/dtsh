f1c8326b095d808fcd9e7b82129920e635dfa6b6
power7865 <2624238128@qq.com>
2024-06-14 12:45:11 +0800

    power注释

--- src/main/java/com/fdzc/springboot01/service/AdminService.java
+++ src/main/java/com/fdzc/springboot01/service/AdminService.java
@@ -16,28 +16,58 @@ public class AdminService {
     @Resource
     AdminMapper adminMapper;
 
+    /**
+     * 查询所有管理员信息。
+     *
+     * @return 包含所有管理员信息的列表。
+     */
     public List<Admin> findAllAdmin() {
         return adminMapper.selectList(null);
     }
 
+    /**
+     * 根据管理员ID查找管理员信息。
+     *
+     * @param id 管理员ID
+     * @return 对应ID的管理员对象，如果不存在则返回 null。
+     */
     public Admin findOneAdmin(Integer id) {
         return adminMapper.selectById(id);
     }
 
+    /**
+     * 添加一个新的管理员。
+     *
+     * @param admin 要添加的管理员对象
+     * @return 添加结果的字符串，格式为 "添加成功：1，添加失败：0"，其中1表示成功，0表示失败。
+     */
     public String addOneAdmin(Admin admin) {
         Integer res = adminMapper.insert(admin);
         return String.format("添加成功：%d，添加失败：%d", res, 1 - res);
     }
 
+    /**
+     * 更新一个现有的管理员信息。
+     *
+     * @param admin 更新后的管理员对象
+     * @return 更新结果的字符串，格式为 "修改成功：1，修改失败：0"，其中1表示成功，0表示失败。
+     */
     public String updateOneAdmin(Admin admin) {
         Integer res = adminMapper.updateById(admin);
         return String.format("修改成功：%d，修改失败：%d", res, 1 - res);
     }
 
+    /**
+     * 删除一个管理员。
+     *
+     * @param id 要删除的管理员的ID
+     * @return 删除结果的字符串，格式为 "删除成功：1，删除失败：0"，其中1表示成功，0表示失败。
+     */
     public String deleteOneAdmin(Integer id) {
         Integer res = adminMapper.deleteById(id);
         return String.format("删除成功：%d，删除失败：%d", res, 1 - res);
     }
+
 
     public Admin findNamePassword(UserDTO userDTO) {
         QueryWrapper<Admin> queryWrapper = new QueryWrapper<>();
--- src/main/java/com/fdzc/springboot01/service/CarbonService.java
+++ src/main/java/com/fdzc/springboot01/service/CarbonService.java
@@ -13,18 +13,41 @@ public class CarbonService {
     @Resource
     UserCarbonMapper userCarbonMapper;
 
+    /**
+     * 查询所有用户碳排放信息。
+     *
+     * @return 包含所有用户碳排放信息的列表。
+     */
     public List<UserCarbon> findAllUserCarbon() {
         return userCarbonMapper.selectList(null);
     }
 
+    /**
+     * 添加一条用户碳排放信息。
+     *
+     * @param userCarbon 要添加的用户碳排放对象
+     * @return 插入操作影响的行数
+     */
     public Integer addOneUserCarbon(UserCarbon userCarbon) {
         return userCarbonMapper.insert(userCarbon);
     }
 
+    /**
+     * 更新一条用户碳排放信息。
+     *
+     * @param userCarbon 更新后的用户碳排放对象
+     * @return 更新操作影响的行数
+     */
     public Integer updateOneUserCarbon(UserCarbon userCarbon) {
         return userCarbonMapper.updateById(userCarbon);
     }
 
+    /**
+     * 删除一条用户碳排放信息。
+     *
+     * @param userCarbon 要删除的用户碳排放对象
+     * @return 删除操作影响的行数
+     */
     public Integer deleteOneUserCarbon(UserCarbon userCarbon) {
         return userCarbonMapper.deleteById(userCarbon);
     }
